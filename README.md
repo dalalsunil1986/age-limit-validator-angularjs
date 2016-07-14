fa2198232944c13dd6a1cf9bc2ef4c03d60a91af
Joel Capillo <Joel Capillo>
2016-07-13 21:47:25 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Age Limit Validator Directive - AngularJs
+
+An AngularJs directive that will validate if minimum age requirement is meet based from the entered date on the textbox.
+
+# How to use?
+
+1. Include the library in your existing library AngularJs library like this?
+    <pre>
+    angular.module('app', ['app.widget'])
+             .controller('YourController', ['$scope', function($scope) {                 
+             }]);
+    </pre>
+
+2. Use the directive on your view like this
+    <pre>
+      <input 
+        ng-model="birthday" 
+        type="text"       
+        age-limit 
+        min-age="18"                                          
+        under-age-msg="You need to be at least 18 years old."
+        title = "Please enter a valid birthdate."                                           
+       >
+    </pre>
---
+++ js/age-limit-validator-directive.js
@@ -0,0 +1,71 @@
+/*
+    Age Limit Validator Directive
+    Copyright (c) 2007 - 2016 Joel Capillo (joecaps.com)
+    Version: 1.0.0
+*/
+/*global angular */
+/*jslint node: true */
+"use strict";
+angular.module('app.widget', [])
+    .directive('ageLimit', function () {
+        /*jslint unparam: true*/
+        return {
+            require: 'ngModel', // get a hold of NgModelController           
+            link: function ($scope, $element, $attrs,  ngModelCtrl) {
+
+                if (!ngModelCtrl) {
+                    throw new Error('ngModel is not set.');
+                }
+
+                var settings = {
+                    minAge: 18,
+                    underAgeMsg: 'You need to be at least 18 years old.',
+                    title: null,
+                    pattern: null
+                },
+                    copyUserSettings = function (attrs, settings) {
+                        var property;
+                        for (property in settings) {
+                            if (settings.hasOwnProperty(property) && attrs.hasOwnProperty(property)) {
+                                settings[property] = attrs[property];
+                            }
+                        }
+                    },
+                    calculateAge = function (birthday, settings) { // birthday is a string 
+                        var regex, today, birthDate, age, month;
+                        if (!settings.pattern) {
+                            regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/; //if no pattern provided
+                            if (!regex.test(birthday)) {
+                                return -1;
+                            }
+                        }
+                        today = new Date();
+                        birthDate = new Date(birthday);
+                        age = today.getFullYear() - birthDate.getFullYear();
+                        month = today.getMonth() - birthDate.getMonth();
+                        if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+                            age = age - 1;
+                        }
+
+                        if (isNaN(age)) {
+                            return -1;  //if age is invalid
+                        }
+
+                        return age;
+                    };
+
+                copyUserSettings($attrs, settings);
+                $element.on('focusout keydown', function (e) {
+                    var age = calculateAge(ngModelCtrl.$viewValue, settings);
+                    e.target.setCustomValidity(""); //clear validation               
+                    if (-1 === age && settings.title) {
+                        e.target.setCustomValidity(settings.title);
+                        return;
+                    }
+                    if (age < parseInt(settings.minAge, 10)) {
+                        e.target.setCustomValidity(settings.underAgeMsg);
+                    }
+                });
+            }
+        };
+    });
